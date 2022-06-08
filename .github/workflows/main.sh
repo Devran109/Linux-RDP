64c743e4a5c4ee022e0ff88b78f52eb0861409ba
Devran109 <88400645+Devran109@users.noreply.github.com>
2022-06-08 10:30:33 +0300

    Update main.sh

--- .github/workflows/main.sh
+++ .github/workflows/main.sh
@@ -6,4 +6,5 @@ sudo apt install lightdm -y
 sudo apt install openbox-gnome-session-y
 sudo apt install gnome-terminal -y
 sudo apt install thunar -y
+sudo apt --fix-broken install
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AX4XfWjKeVJmQW9B8Aqg1uMlHKaotGf2Yzk3k1sFPKlWhIHUP2a77qLEQRTM0poAIBMveQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
